a8ce5c93bf690edba4804bd5232b704423f30ef0
Beatriz Lopes de Almeida <bialopes2610@gmail.com>
2026-09-04 11:54:37 -0300

    Add files via upload

---
+++ desafio1.py
@@ -0,0 +1,5 @@
+n1 = int(input("Digite o primeiro numero inteiro: "))
+n2 = int(input("Digite o segundo numero inteiro: "))
+multiplicacao = n1 * n2
+
+print("O resultado da multiplicacao é: ", multiplicacao)
